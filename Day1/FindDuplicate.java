2bf3a75360bc74ee3e666421aba479466607f193
Abhishek Mishra <cs.abhishekmo@gmail.com>
2024-11-08 15:17:52 +0530

    test

--- Day1/FindDuplicate.java
+++ Day1/FindDuplicate.java
@@ -1,5 +1,5 @@
 class Solution {
-    public int findDuplicate(int[] nums) {
+    public int findDuplicate1(int[] nums) {
         for(int i = 0; i < nums.length; i++) {
             int x = Math.abs(nums[i]);
             if(nums[x] < 0) {
@@ -10,5 +10,24 @@ class Solution {
         }
 
         return -1;
+    }
+
+    //Slow and Fast pointers
+    public int findDuplicate2(int[] nums) {
+        int slow = nums[0];
+        int fast = nums[0];
+
+        do {
+            slow = nums[slow];
+            fast = nums[nums[fast]];
+        } while (nums[slow] != nums[fast]);
+
+        slow = nums[0];
+        while(slow != fast) {
+            slow = nums[slow];
+            fast = nums[fast];
+        }
+
+        return slow;
     }
 }
